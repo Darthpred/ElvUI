88861f3e309581874d951cbf35c7b074499b083f
Darthpred <darthpred@gmail.com>
2012-02-23 12:43:36 +0400

    Добавление тестового инфо-текста версии.

---
+++ modules/datatexts/version.lua
@@ -0,0 +1,46 @@
+﻿local E, L, P, G = unpack(select(2, ...)); --Inport: Engine, Locales, ProfileDB, GlobalDB
+local DT = E:GetModule('DataTexts')
+
+local displayString = '';
+local lastPanel;
+
+local function Update(self, t)
+
+end
+
+local function OnEvent(self, event, ...)
+	lastPanel = self
+	self.text:SetFormattedText(displayString, 'ElvUI v', 3.22);
+end
+
+local function Click()
+
+end
+
+local function OnEnter(self)
+	--DT:SetupTooltip(self)
+
+	--GameTooltip:Show()
+end
+
+local function ValueColorUpdate(hex, r, g, b)
+	displayString = string.join("", "%s", hex, "%.2f|r", "-alpha : Darth Predator's edit")
+	
+	if lastPanel ~= nil then
+		OnEvent(lastPanel)
+	end
+end
+E['valueColorUpdateFuncs'][ValueColorUpdate] = true
+--[[
+	DT:RegisterDatatext(name, events, eventFunc, updateFunc, clickFunc, onEnterFunc, onLeaveFunc)
+	
+	name - name of the datatext (required)
+	events - must be a table with string values of event names to register 
+	eventFunc - function that gets fired when an event gets triggered
+	updateFunc - onUpdate script target function
+	click - function to fire when clicking the datatext
+	onEnterFunc - function to fire OnEnter
+	onLeaveFunc - function to fire OnLeave, if not provided one will be set for you that hides the tooltip.
+]]
+DT:RegisterDatatext("Версия", {'PLAYER_ENTERING_WORLD'}, OnEvent, Update, Click, OnEnter)
+
