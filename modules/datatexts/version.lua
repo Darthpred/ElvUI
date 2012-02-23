e505efd9b0e057e0aed29f36ab271458603b18a3
Darthpred <darthpred@gmail.com>
2012-02-23 13:30:19 +0400

    Смена формата строк подсказки инфо-текста. Доудаление лишнего.

--- modules/datatexts/version.lua
+++ modules/datatexts/version.lua
@@ -18,13 +18,13 @@ local function OnEnter(self)
 	DT:SetupTooltip(self)
 
 	GameTooltip:AddDoubleLine("Сборка ElvUI версии", E.version)
-	GameTooltip:AddDoubleLine("Редакция от Darth Predator (Дартпредатор - Свежеватель Душ)")
+	GameTooltip:AddLine("Редакция от Darth Predator (Дартпредатор - Свежеватель Душ)")
 	GameTooltip:AddLine(" ")
 	GameTooltip:AddLine("При возникновении вопросов или желании подогнать сборку под себя обращаться:")
-	GameTooltip:AddDoubleLine("- Игровая почта")
-	GameTooltip:AddDoubleLine("- Личное сообщение на форуме TukUI, ник Darth Predator")
-	GameTooltip:AddDoubleLine("- Личное сообщение на форуме Curse, ник Darth_Predator")
-	GameTooltip:AddDoubleLine("- Сайт Shadowmage.ru")
+	GameTooltip:AddLine("- Игровая почта")
+	GameTooltip:AddLine("- Личное сообщение на форуме TukUI, ник Darth Predator")
+	GameTooltip:AddLine("- Личное сообщение на Curse, ник Darth_Predator")
+	GameTooltip:AddLine("- Сайт Shadowmage.ru")
 	
 	GameTooltip:Show()
 end
@@ -37,16 +37,6 @@ local function ValueColorUpdate(hex, r, g, b)
 	end
 end
 E['valueColorUpdateFuncs'][ValueColorUpdate] = true
---[[
-	DT:RegisterDatatext(name, events, eventFunc, updateFunc, clickFunc, onEnterFunc, onLeaveFunc)
-	
-	name - name of the datatext (required)
-	events - must be a table with string values of event names to register 
-	eventFunc - function that gets fired when an event gets triggered
-	updateFunc - onUpdate script target function
-	click - function to fire when clicking the datatext
-	onEnterFunc - function to fire OnEnter
-	onLeaveFunc - function to fire OnLeave, if not provided one will be set for you that hides the tooltip.
-]]
+
 DT:RegisterDatatext("Сборка", {'PLAYER_ENTERING_WORLD'}, OnEvent, Update, Click, OnEnter)
 
