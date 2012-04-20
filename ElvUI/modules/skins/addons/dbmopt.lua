432d14372e3d4dc25ade2cdba486d47a504b8b72
Darthpred <darthpred@gmail.com>
2012-04-20 12:38:16 +0400

    Скин для опций ДБМ. Не полный. Я ненавижу скинить фреймы, сделанные на xml, это почти невозможно >_<

---
+++ ElvUI/modules/skins/addons/dbmopt.lua
@@ -0,0 +1,18 @@
+local E, L, P, G = unpack(select(2, ...)); --Inport: Engine, Locales, ProfileDB, GlobalDB
+local S = E:GetModule('Skins')
+
+local function LoadSkin()
+	DBM_GUI_OptionsFrame:StripTextures()
+	DBM_GUI_OptionsFrameBossMods:StripTextures()
+	DBM_GUI_OptionsFrameDBMOptions:StripTextures()
+	DBM_GUI_OptionsFrame:SetTemplate("Transparent")
+	DBM_GUI_OptionsFrameBossMods:SetTemplate("Transparent")
+	DBM_GUI_OptionsFrameDBMOptions:SetTemplate("Transparent")
+	DBM_GUI_OptionsFramePanelContainer:SetTemplate("Transparent")
+	
+	S:HandleTab(DBM_GUI_OptionsFrameTab1)
+	S:HandleTab(DBM_GUI_OptionsFrameTab2)
+	S:HandleButton(DBM_GUI_OptionsFrameOkay, true)
+end
+
+S:RegisterSkin('DBM-GUI', LoadSkin)
