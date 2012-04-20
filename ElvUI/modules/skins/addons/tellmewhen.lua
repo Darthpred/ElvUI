d64a24d3fb0e817475ad7b88af461a6254f1da65
Darthpred <darthpred@gmail.com>
2012-04-20 13:55:49 +0400

    Дополнения к скину TMW

--- ElvUI/modules/skins/addons/tellmewhen.lua
+++ ElvUI/modules/skins/addons/tellmewhen.lua
@@ -22,6 +22,43 @@ local function LoadSkin()
 
 	S:HandleButton(TellMeWhen_IconEditorOkay, true)
 	S:HandleCloseButton(TellMeWhen_IconEditorClose, true)
+	
+	TellMeWhen_ConfigWarning:StripTextures()
+	TellMeWhen_ConfigWarning:SetTemplate("Transparent")
+	S:HandleButton(TellMeWhen_ConfigWarningExit, true)
+	S:HandleButton(TellMeWhen_ConfigWarningNeverAgain, true)
+	
+	
+	TellMeWhen_IconEditorSuggest:ClearAllPoints()
+	TellMeWhen_IconEditorSuggestItem1:ClearAllPoints()
+	TellMeWhen_IconEditorSuggestItem1:SetSize(200, 20)
+	TellMeWhen_IconEditorSuggest:SetSize(210, 400)
+	TellMeWhen_IconEditorSuggest:SetTemplate("Transparent")
+	TellMeWhen_IconEditorSuggest:Point("LEFT", TellMeWhen_IconEditorMain, "RIGHT", 1, 0)
+	TellMeWhen_IconEditorSuggestItem1:Point("TOPLEFT", TellMeWhen_IconEditorSuggest, "TOPLEFT", 5, -38)
+	
+	TellMeWhen_IconEditorTab1:ClearAllPoints()
+	TellMeWhen_IconEditorTab2:ClearAllPoints()
+	TellMeWhen_IconEditorTab3:ClearAllPoints()
+	TellMeWhen_IconEditorTab4:ClearAllPoints()
+	TellMeWhen_IconEditorTab5:ClearAllPoints()
+	
+	TellMeWhen_IconEditorTab1:SetSize(89, 30)
+	TellMeWhen_IconEditorTab2:SetSize(94, 30)
+	TellMeWhen_IconEditorTab3:SetSize(86, 30)
+	TellMeWhen_IconEditorTab4:SetSize(112, 30)
+	TellMeWhen_IconEditorTab5:SetSize(210, 30)
+	
+	TellMeWhen_IconEditorTab1:Point("TOPLEFT", TellMeWhen_IconEditorMain, "BOTTOMLEFT", 0, 4)
+	TellMeWhen_IconEditorTab2:Point("LEFT", TellMeWhen_IconEditorTab1, "RIGHT", -19, 0)
+	TellMeWhen_IconEditorTab3:Point("LEFT", TellMeWhen_IconEditorTab2, "RIGHT", -19, 0)
+	TellMeWhen_IconEditorTab4:Point("LEFT", TellMeWhen_IconEditorTab3, "RIGHT", -19, 0)
+	TellMeWhen_IconEditorTab5:Point("LEFT", TellMeWhen_IconEditorTab4, "RIGHT", -19, 0)
+	
+	S:HandleScrollBar(TellMeWhen_IconEditorEventsSoundSoundsScrollBar)
+	S:HandleCheckBox(TellMeWhen_IconEditorEventsEventSettingsPassThrough)
+	S:HandleCheckBox(TellMeWhen_IconEditorEventsEventSettingsOnlyShown)
+	
 end
 
 S:RegisterSkin("TellMeWhen_Options", LoadSkin)
